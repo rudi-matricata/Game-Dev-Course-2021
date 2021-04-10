c6d084a1feb7d279ba8f5c3e852e53984cd490eb
Radoslav Filipov <radoslav.filipov@sap.com>
2021-04-10 19:42:04 +0300

    Add keys / hearts stats support

--- Homework-1/Assets/Scripts/HeroStats.cs
+++ Homework-1/Assets/Scripts/HeroStats.cs
@@ -4,16 +4,34 @@ using UnityEngine;
 
 public class HeroStats : MonoBehaviour
 {
+    private const string heroKeysTag = "HeroKeys";
+    private const string fullKeySpriteName = "HudKeyFull";
+
     private int obtainedKeysCount;
+
+    private readonly SpriteRenderer[] keys = new SpriteRenderer[3];
 
     private void Start() {
         obtainedKeysCount = 0;
 
         HeroCollisions.OnKeyObtained += IncreaseObtainedKeys;
+
+        foreach (Transform child in transform) {
+            if (child.CompareTag(heroKeysTag)) {
+                int i = 0;
+                foreach(Transform keyTransform in child.transform) {
+                    keys[i++] = keyTransform.GetComponent<SpriteRenderer>();
+                }
+            }
+        }
     }
 
     private void IncreaseObtainedKeys() {
-        obtainedKeysCount++;
+        if(obtainedKeysCount >= 3) {
+            return;
+        }
+        keys[obtainedKeysCount++].sprite = Resources.Load<Sprite>(fullKeySpriteName);
+
         Debug.Log("Obtained keys: " + obtainedKeysCount);
     }
 }
