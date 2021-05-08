79393c662f9265fa3b8c46cc8f8a597876905c87
Radoslav Filipov <radoslav.filipov@sap.com>
2021-05-09 00:24:40 +0300

    use taw images for hero bars

---
+++ Homework/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour {
+
+    private const string heartTag = "Heart";
+
+    private readonly Stack<GameObject> hearts = new Stack<GameObject>();
+
+    void Start() {
+        foreach (Transform child in transform) {
+            if (child.CompareTag(heartTag)) {
+                hearts.Push(child.gameObject);
+            }
+        }
+        HeroCollisions.OnEnemyHit += LoseHeart;
+    }
+
+    private void OnDestroy() {
+        HeroCollisions.OnEnemyHit -= LoseHeart;
+    }
+
+    private void LoseHeart() {
+        if (hearts.Count > 0) {
+            Destroy(hearts.Pop());
+        } else {
+            Debug.Log("No more hearts");
+        }
+    }
+}
---
+++ Homework/Assets/Scripts/KeysBar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeysBar : MonoBehaviour {
+
+    private const string heroKeyTag = "HeroKey";
+    private const string fullKeySpriteName = "HudKeyFull";
+
+    private int obtainedKeysCount;
+
+    private RawImage[] keys = new RawImage[3];
+    private Texture2D fullKeySprite;
+
+    private void Start() {
+        obtainedKeysCount = 0;
+        fullKeySprite = Resources.Load<Texture2D>(fullKeySpriteName);
+
+        HeroCollisions.OnKeyObtained += IncreaseObtainedKeys;
+
+        int i = 0;
+        foreach (Transform child in transform) {
+            if (child.CompareTag(heroKeyTag)) {
+                keys[i++] = child.GetComponent<RawImage>();
+            }
+        }
+    }
+
+    private void OnDestroy() {
+        HeroCollisions.OnKeyObtained -= IncreaseObtainedKeys;
+    }
+
+    private void IncreaseObtainedKeys() {
+        if (obtainedKeysCount >= 3) {
+            return;
+        }
+        keys[obtainedKeysCount++].texture = fullKeySprite;
+
+        Debug.Log("Obtained keys: " + obtainedKeysCount);
+    }
+}
