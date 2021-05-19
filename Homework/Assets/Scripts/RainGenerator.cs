dd59a39e89831d674a84045fd4e7665bc6fb2581
Radoslav Filipov <radoslav.filipov@sap.com>
2021-05-19 19:59:14 +0300

    add rain

---
+++ Homework/Assets/Scripts/RainGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainGenerator : MonoBehaviour
+{
+
+    private ParticleSystem ps;
+
+    private float nextRainEventTime;
+    private float timeDuration;
+
+    [SerializeField]
+    private float maxTimeInSeconds;
+    [SerializeField]
+    private float minTimeInSeconds;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        ps.enableEmission= false;
+
+        timeDuration = 0f;
+        maxTimeInSeconds = 60f;
+        minTimeInSeconds = 45f;
+        setNextRainEvent();
+    }
+
+    void Update()
+    {
+        timeDuration += Time.deltaTime;
+
+        if(timeDuration >= nextRainEventTime) {
+            timeDuration = 0f;
+            setNextRainEvent();
+
+            ps.enableEmission = !ps.enableEmission;
+        }
+    }
+
+    private void setNextRainEvent() {
+        nextRainEventTime = Random.Range(minTimeInSeconds, maxTimeInSeconds);
+    }
+}
