8c03ee4025820469c7273e912cdcb9faee52cd47
Radoslav Filipov <radoslav.filipov@sap.com>
2021-05-20 09:54:28 +0300

    Add rigidbody material

--- Homework/Assets/Scripts/HeroCollisions.cs
+++ Homework/Assets/Scripts/HeroCollisions.cs
@@ -23,7 +23,7 @@ public class HeroCollisions : MonoBehaviour {
     private Rigidbody2D rigidBody;
 
     private void Start() {
-        trampolineForceFactor = 11;
+        trampolineForceFactor = 7;
 
         rigidBody = GetComponent<Rigidbody2D>();
         rigidBody.freezeRotation = true;
